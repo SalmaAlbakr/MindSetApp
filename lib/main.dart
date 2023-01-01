f61b1af01fc06959288a1a352fa792cf5f109cd6
merna <gamalmerna549@gmail.com>
2023-01-02 01:07:58 +0200

    merna

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(child: Container()),
+    );
   }
 }
