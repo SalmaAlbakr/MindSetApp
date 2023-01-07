3993f72bd79b05321cf6a6287db74ef9e454fafe
SalmaAlbakr <salalbakr19988@gmail.com>
2023-01-08 01:17:14 +0200

    calculator app

---
+++ lib/note_screen.dart
@@ -0,0 +1,106 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List todos = [];
+  String input = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffEFC5C5),
+      appBar: AppBar(
+        backgroundColor: Color(0xffF05151),
+        elevation: 0.0,
+        title: Text("My ToDo",
+          style: TextStyle(
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white
+          ),),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            showDialog(
+                context: context,
+                builder: (BuildContext context)
+                {
+                  return AlertDialog(
+                    elevation: 4.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)
+                    ),
+                    title: Text("Add To Do List"),
+                    content: TextField(
+                      onChanged: (String value){
+                        input = value;
+                      },
+                    ),
+                    actions: <Widget>[
+                      // ignore: deprecated_member_use
+                      ElevatedButton(
+                          onPressed: (){
+                            setState(() {
+                              todos.add(input);
+                            });
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Add "))
+                    ],
+                  );
+                }
+            );
+          },
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          )
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (BuildContext context,int index){
+              return Dismissible(
+                key: Key(todos[index]),
+                child: Card(
+                  elevation:  4,
+                  margin: EdgeInsets.all(8),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)
+                  ),
+                  child: ListTile(
+                    title: Text(todos[index]),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete,
+                          color: Colors.red),
+                      onPressed: (){
+                        setState(() {
+                          todos.removeAt(index);
+                        });
+                      },
+                    ),
+                  ),
+                ),);
+            }),
+      ),
+    );
+  }
+}
