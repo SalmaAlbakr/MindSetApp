5e48243cdd768be3926da6635ffc5fd4e82d70f8
SalmaAlbakr <salalbakr19988@gmail.com>
2023-03-22 23:28:41 +0200

    calc

--- lib/home/splash_screen.dart
+++ lib/home/splash_screen.dart
@@ -1,11 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-import 'package:next_poject/calculator/calculator_screen.dart';
 import 'package:next_poject/home/first_screen.dart';
-import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:next_poject/main.dart';
-import 'package:next_poject/reminder/notification_controller.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -15,20 +12,23 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  var timer = Timer (Duration(seconds: 4),(){
-    MyApp.navigatorKey.currentState?.pushReplacement(MaterialPageRoute(builder: (_) => MainPage()));
-  });
+  var timer = Timer(
+    Duration(seconds: 4),
+    () {
+      MyApp.navigatorKey.currentState?.pushReplacement(
+        MaterialPageRoute(
+          builder: (_) => MainPage(),
+        ),
+      );
+    },
+  );
 
   @override
   void initState() {
     super.initState();
-    // Timer (Duration(seconds: 4),(){
-    //   // MyApp.navigatorKey.currentState?.push(MaterialPageRoute(builder: (_) => MainPage()));
-    //   // Navigator.pushReplacement(
-    //   //     context , MaterialPageRoute(builder: (context) => MainPage()));
-    // });
     timer;
   }
+
   @override
   void dispose() {
     timer;
@@ -39,8 +39,8 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body:Center(
-          child: Lottie.asset("assets/98050-creativity.json")
+      body: Center(
+        child: Lottie.asset("assets/98050-creativity.json"),
       ),
     );
   }
